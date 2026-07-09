32351682dd47f9cebbfc475dcd3683b8410c6c85
NKrut <nataly6499334@gmail.com>
2026-07-09 11:58:54 +0300

    Task_3 исправления замечаний

--- src/test/java/ConstructorTests.java
+++ src/test/java/ConstructorTests.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.MainPage;
